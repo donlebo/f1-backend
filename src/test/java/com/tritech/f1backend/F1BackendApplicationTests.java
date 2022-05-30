d9362e513c1e7bac8ca6cb2cfa8f2cecc294753b
salupo <saluposebastiano@gmail.com>
2022-05-30 17:06:48 +0200

    initial commit

---
+++ src/test/java/com/tritech/f1backend/F1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tritech.f1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
